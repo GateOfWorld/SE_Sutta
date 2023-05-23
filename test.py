2eea69adbd4140273539c23713cba6fb8b020c90
Lee Harim <101526662+GateOfWorld@users.noreply.github.com>
2023-05-23 13:29:44 +0900

    test2

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ class HT_Deck():
     def __init__(self) -> None:
         pass
     
-    def pop_card(self):
+    def pop_deck(self):
+        pass
+    
+    def return_deck(self):
         pass
     
