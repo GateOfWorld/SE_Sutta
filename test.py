758519631dbecd3ade01394c5353ed73c04c057d
Lee Harim <101526662+GateOfWorld@users.noreply.github.com>
2023-05-24 05:02:45 +0000

    2305241402

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-import random
-
 class SuttaDeck:
     def __init__(self)->None:
+        import random
         self.deck=[]
         for i in range(1,11):
             self.deck.append((i,'a'))
@@ -11,10 +10,10 @@ class SuttaDeck:
 
     def pop_deck(self, card_num:int=1):
         if card_num==1:
-            card_res = self.deck.pop()
+            card_res:tuple = self.deck.pop()
         else :
             card_res:list = []
-            for i in range(card_num):
+            for _ in range(card_num):
                 card_res.append(self.deck.pop())
         return card_res
     
@@ -32,18 +31,22 @@ class SuttaPlayer:
     def __init__(self, userdata:str="") -> None:
         if userdata!="":
             pass            ##불러오기
-            self.data:str = ""
-            self.hand:list = []
-            self.money:int = 100000
-            self.wp:int = 0
-            self.lp:int = 0
         else:
             self.data:str = ""
             self.hand:list = []
             self.money:int = 100000
             self.wp:int = 0
             self.lp:int = 0
-        return
+            
+    def get_card(self, card):
+        if str(type(card))=="<class 'list'>":
+            self.hand.__iadd__(card)
+            return True
+        elif str(type(card))=="<class 'tuple'>":
+            self.hand.append(card)
+            return True
+        return False
+    
     
     def win(self, gain_money:int)->None:
         self.wp+=1
@@ -86,11 +89,11 @@ class SuttaPlayer:
                 return 1#47암행, 1끗취급
             elif self.hand[0][0]==3 and self.hand[1][0]==7:
                 return 6#37땡잡이, 망통취급
-            else : return lowpanjeong()
+            else : return lowpanjeong(self)
         elif self.hand[0][0]==self.hand[1][0]:
             if self.hand[0][0]==10 : return 4
             else : return 16-self.hand[0][0]
-        else : return lowpanjeong()
+        else : return lowpanjeong(self)
     
     def bet_player(self, bet_money:int):
         if self.money<bet_money:
@@ -109,21 +112,19 @@ class Game:
         for i in range(player):
             self.player.append(SuttaPlayer(userdata[i]))        
 
-    def Do_Sutta_Game(self, start_money:int=100):
-        pass
-
-    def top_grade(self):
-        rank = []
-        for i in range(len(self.player)):
-            self.player[i].sort()
-            rank[i] = { 'p_no':i, 'grade':self.player[i].grade(), 'win':False}
-        rank.sort(lambda x:x[i]["grade"])
-        if rank[0]['grade']==1 and rank[1]['grade']>3:
-            rank[0]['grade']==38
-        elif rank[0]['grade']==10 and rank[1]['grade']>19 : 
-            rank[0]['grade']==39
-        rank.sort(lambda x:x[i]["grade"])
-        return rank
-
+    def Do_Sutta_Game(self, start_money:int=0):
+        pandon:int = start_money
+        if pandon==0:
+            for i in range(len(self.player)):
+                pandon+=self.player[i].bet_player(1000)
+            for p in self.player:
+                pp:SuttaPlayer = p
+                pp.get_card(self.deck.pop_deck())
+        else :
+            pass
+        
+        
 if __name__=="__main__":
     g = Game(5,['','','','',''])
+    g.Do_Sutta_Game()
+    print()
